57aed82c786207361ec65e5ec748b4f8b3399378
echizenyayota <virtualmisso30@gmail.com>
2018-07-17 10:26:20 +0900

    プロパティ・コンストラクタ・設定ページ表示メソッドの定義

--- wp-content/plugins/PayPal_ExpressCheckout/PayPal_ExpressCheckout.php
+++ wp-content/plugins/PayPal_ExpressCheckout/PayPal_ExpressCheckout.php
@@ -15,6 +15,39 @@ Text Domain: paypal_expresscheckout
 
 class PayPal_ExpressCheckout {
 
+  // プロパティ
+  private $options;
+
+  // コンストラクタ
+  public function __construct() {
+    add_action( 'admin_menu', array($this, 'paypalexpresscheckout_add_admin_menu') );
+  }
+
+  // ダッシュボードにサブメニューを表示するメソッド
+  public function paypalexpresscheckout_add_admin_menu() {
+      add_options_page(
+        'Settings Admin',
+        'PayPal ExpressCheckout',
+        'manage_options',
+        'my-setting-admin',
+        array( $this, 'create_admin_page' )
+    );
+  }
+
+  public function create_admin_page() {
+    // my_option_nameをoptionsのプロパティとする
+    $this->options = get_option( 'paypl_option_name' );
+    ?>
+    <div class="wrap">
+       <h2>PayPal ExpressCheckout Settings</h2>
+       <form method="post" action="options.php">
+         <?php settings_fields( 'paypal-settings-group' ); ?>
+         <?php do_settings_sections( 'paypal-settings-group' ); ?>
+         <?php submit_button(); ?>
+       </form>
+    </div>
+    <?php
+  }
 }
 
 require(__DIR__ . '/PayPal_ExpressCheckout_admin.php');
