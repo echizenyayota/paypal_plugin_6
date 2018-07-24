21ef7c2cc4c705ee9a058d0287516b92b5c2c3d5
echizenyayota <virtualmisso30@gmail.com>
2018-07-24 09:33:11 +0900

    設定ページの初期化

--- wp-content/plugins/PayPal_ExpressCheckout/PayPal_ExpressCheckout.php
+++ wp-content/plugins/PayPal_ExpressCheckout/PayPal_ExpressCheckout.php
@@ -12,17 +12,14 @@ Author URI: https://e-yota.com
 License: GPLv2 or later
 Text Domain: paypal_expresscheckout
 */
-
 class PayPal_ExpressCheckout {
-
   // プロパティ
   private $options;
-
   // コンストラクタ
   public function __construct() {
     add_action( 'admin_menu', array($this, 'paypalexpresscheckout_add_admin_menu') );
+    add_action( 'admin_init', array( $this, 'paypal_init' ) );
   }
-
   // ダッシュボードにサブメニューを表示するメソッド
   public function paypalexpresscheckout_add_admin_menu() {
       add_options_page(
@@ -33,7 +30,6 @@ class PayPal_ExpressCheckout {
         array( $this, 'create_admin_page' )
     );
   }
-
   public function create_admin_page() {
     // paypal_option_nameをoptionsのプロパティとする
     $this->options = get_option( 'paypal_option_name' );
@@ -48,6 +44,63 @@ class PayPal_ExpressCheckout {
     </div>
     <?php
   }
+ public function paypal_init() {
+    // Option groupとOption nameの登録
+    register_setting(
+        'paypal-settings-group', // Option group
+        'paypal_option_name', // Option name
+        array( $this, 'sanitize' ) // Sanitize
+    );
+    add_settings_section(
+      'setting_section_id', // ID
+      'PayPal ExpressCheckout Settings', // Title
+      array( $this, 'print_section_info' ), // Callback
+      'paypal-settings-group' // Page
+    );
+    add_settings_field(
+      'env', // ID
+      'Enviroment', // Title
+      array( $this, 'enviroment_callback' ), // Callback
+      'paypal-settings-group', // Page
+      'setting_section_id' // Section
+    );
+    add_settings_field(
+      'client', // ID
+      'cleint ID', // Title
+      array( $this, 'client_callback' ), // Callback
+      'paypal-settings-group', // Page
+      'setting_section_id' // Section
+    );
+  }
+  // 入力項目のサニタイズ
+  public function sanitize( $input ) {
+    $new_input = array();
+    if( isset( $input['client'] ) ) {
+      $new_input['client'] = sanitize_text_field( $input['client'] );
+    }
+    return $new_input;
+  }
+  // 記入案内の文字列を表示
+  public function print_section_info() {
+    print 'Enter your settings below:';
+  }
+  // 実行環境の選択
+  public function enviroment_callback() {
+    ?>
+      <p>
+        <select name="env" size="1">
+          <option value="sandbox">sandbox</option>
+          <option value="production">production</option>
+        </select>
+      </p>
+    <?php
+  }
+  // client IDの入力
+  public function client_callback() {
+    printf(
+            '<input type="text" id="title" name="paypal_option_name[client]" size="90" value="%s" />',
+            isset( $this->options['client'] ) ? esc_attr( $this->options['client']) : ''
+          );
+  }
 }
-
 require(__DIR__ . '/PayPal_ExpressCheckout_admin.php');
