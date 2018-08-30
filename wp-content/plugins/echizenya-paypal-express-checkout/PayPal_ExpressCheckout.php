b1be55743175b38632cd080600ebd8598412db0f
echizenyayota <virtualmisso30@gmail.com>
2018-08-30 12:06:19 +0900

    プラグインのヘッダー部分を変更

--- wp-content/plugins/echizenya-paypal-express-checkout/PayPal_ExpressCheckout.php
+++ wp-content/plugins/echizenya-paypal-express-checkout/PayPal_ExpressCheckout.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * @package express
- */
-/*
-Plugin Name: Echizenya PayPal Express Checkout
-Plugin URI: https://example.com
-Description: PayPal Express Checkout
-Version: 0.0.0
-Author: echizenya
-Author URI: https://e-yota.com
-License: GPLv2 or later
-Text Domain: paypal_expresscheckout
-*/
 class PayPal_ExpressCheckout {
   // プロパティ
   private $options;
--- wp-content/plugins/echizenya-paypal-express-checkout/PayPal_ExpressCheckout_admin.php
+++ wp-content/plugins/echizenya-paypal-express-checkout/PayPal_ExpressCheckout_admin.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * @package express
+ */
+/*
+Plugin Name: Echizenya PayPal Express Checkout
+Plugin URI: https://example.com
+Description: PayPal Express Checkout
+Version: 0.0.0
+Author: echizenya
+Author URI: https://e-yota.com
+License: GPLv2 or later
+Text Domain: paypal_expresscheckout
+*/
 if( is_admin() ){
   $pypl_expr = new PayPal_ExpressCheckout();
 }
